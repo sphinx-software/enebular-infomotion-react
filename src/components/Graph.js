fed48646d0cfc9e8406e374dece7c5148711145c
mickey-ichi <lemanhhungb01501@gmail.com>
2017-10-10 16:05:38 +0700

    update graph

---
+++ src/components/Graph.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {infomotionUI} from 'enebular-infomotion'
+
+class Graph extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        let graph = infomotionUI.graph(this.graphElement, this.props.graphSetting);
+        graph.render();
+
+        this.props.onReady(graph)
+    }
+
+    render() {
+        return <div style={{height: '100%'}} ref={(element) => {
+            this.graphElement = element
+        }}/>
+    }
+}
+
+Graph.propTypes = {
+    graphSetting: PropTypes.object.isRequired,
+    onReady: PropTypes.func.isRequired,
+};
+
+Graph.defaultProps = {
+    onReady: {},
+    graphSetting: {},
+};
+
+export default Graph
