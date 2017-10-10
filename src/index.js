1aa80f21553a5351b0d3f9b70c3c4a93b32f2ae5
mickey-ichi <lemanhhungb01501@gmail.com>
2017-10-10 16:09:03 +0700

    update graph

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Timeline from './components/Timeline';
 import Datepicker from './components/Datepicker';
 import LiveButton from './components/LiveButton';
+import Graph from './components/Graph';
 
-export {Timeline, Datepicker, LiveButton};
+export {Timeline, Datepicker, LiveButton, Graph};
